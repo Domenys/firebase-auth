82886f16214272d09902fe75fdcb7632ae6270a8
Tascan <34191135+Domenys@users.noreply.github.com>
2021-05-28 01:15:45 +0300

    Added Sign in form

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,18 @@
-import Home from './views/Home.vue'
-import About from './views/About.vue'
-import NotFound from './views/NotFound.vue'
+import Home from '@/views/Home.vue'
+import Auth from '@/views/Auth.vue'
+import NotFound from '@/views/NotFound.vue'
 
 /** @type {import('vue-router').RouterOptions['routes']} */
 export const routes = [
-  { path: '/', component: Home, meta: { title: 'Home' } },
-  {
-    path: '/about',
-    meta: { title: 'About' },
-    component: About,
-    // example of route level code-splitting
-    // this generates a separate chunk (About.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    // component: () => import('./views/About.vue')
+  { 
+    path: '/', 
+    component: Home, 
+    name: 'Home' 
+  },
+  { 
+    path: '/auth', 
+    component: Auth, 
+    name: 'Auth' 
   },
   { path: '/:path(.*)', component: NotFound },
 ]
--- src/views/About.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    321
-</template>
