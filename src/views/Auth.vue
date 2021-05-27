68d954155a3f87c96b3f11f23649b3a049df2b70
Tascan <34191135+Domenys@users.noreply.github.com>
2021-05-28 02:20:47 +0300

    Added vueuse core

--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -30,7 +30,7 @@
                             </path>
                         </svg>
                     </span>
-                    <input type="text" id="sign-in-email" class=" rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Ваш email"/>
+                    <input type="email" v-model="authData.email" id="sign-in-email" class=" rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Ваш email"/>
                     </div>
                 </div>
                 <div class="flex flex-col mb-6">
@@ -41,7 +41,7 @@
                                 </path>
                             </svg>
                         </span>
-                        <input type="password" id="sign-in-email" class=" rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Ваш пароль"/>
+                        <input type="password" v-model="authData.password" id="sign-in-email" class=" rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Ваш пароль"/>
                         </div>
                     </div>
                     <div class="flex items-center mb-6 -mt-4">
@@ -52,7 +52,7 @@
                         </div>
                     </div>
                     <div class="flex w-full">
-                        <button type="submit" class="py-2 px-4  bg-purple-600 hover:bg-purple-700 focus:ring-purple-500 focus:ring-offset-purple-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
+                        <button @click="signIn" type="button" class="py-2 px-4  bg-purple-600 hover:bg-purple-700 focus:ring-purple-500 focus:ring-offset-purple-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
                             Login
                         </button>
                     </div>
@@ -72,8 +72,21 @@
 </template>
 
 <script>
+  import { reactive } from 'vue'
 export default {
-  name: 'Auth'
+  setup() {
+    const authData = reactive( { email: '', password: '' } )
+
+    const signIn = () => {
+      console.log('EMAIL', authData.email)
+      console.log('PASSWORD', authData.password)
+    } 
+
+    return {
+      authData,
+      signIn
+    }
+  }
 }
 </script>
 
